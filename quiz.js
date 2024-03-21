91a749ed4fb1a5ecfa68ac8feee2a51ac902f92c
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-21 13:27:21 -0400

    Program 1 and 2

--- quiz.js
+++ quiz.js
@@ -1,8 +1,20 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
+let input_temp = prompt("Enter a temperature in fahrenheit.");
+let output_temp = (input_temp - 32) * (5/9)
+console.log(input_temp,"degrees fahrenheit is equal to", output_temp, "degrees celsius");
 
 //Program 2: Grade Calculator
+let grade_percentage = prompt("Enter your grade in percentage.");
+if (grade_percentage >= 90) {
+    console.log("Your percentage grade of", grade_percentage, "corresponds to an A")
+}
+else if (89>= grade_percentage >= 80){
+    console.log("Your percentage grade of", grade_percentage, "corresponds to a B")
+}
+
+
 
 //Program 3: Leap Year Checker
 
