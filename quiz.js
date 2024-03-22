dd108518f9420952ce592cc63fd5b44a0c04ec1a
erickmartins7 <159183580+erickmartins7@users.noreply.github.com>
2024-03-22 14:26:09 -0400

    Final

--- quiz.js
+++ quiz.js
@@ -1,27 +1,54 @@
 const prompt = require('prompt-sync')();
 
 //Program 1: Temperature Converter
-let input_temp = prompt("Enter a temperature in fahrenheit.");
+let input_temp = prompt("Enter a temperature in fahrenheit: ");
 let output_temp = (input_temp - 32) * (5/9)
 console.log(input_temp,"degrees fahrenheit is equal to", output_temp, "degrees celsius");
 
 //Program 2: Grade Calculator
 let grade_percentage = prompt("Enter your grade in percentage.");
 if (grade_percentage >= 90) {
-    console.log("Your percentage grade of", grade_percentage, "corresponds to an A");
+    console.log("Your percentage grade corresponds to an A");
 }
-else if (89>= grade_percentage >= 80){
-    console.log("Your percentage grade of", grade_percentage, "corresponds to a B");
+else if (grade_percentage >= 80){
+    console.log("Your percentage grade corresponds to a B");
 }
-else if (79>= grade_percentage >= 70){
-    console.log("Your percentage grade of", grade_percentage, "corresponds to a C");
+else if (grade_percentage >= 70){
+    console.log("Your percentage grade corresponds to a C");
 }
-else if (69>= grade_percentage >= 60){
-    console.log("Your percentage grade of", grade_percentage, "corresponds to a D");
+else if (grade_percentage >= 60){
+    console.log("Your percentage grade corresponds to a D");
 }
-else if (grade_percentage < 60){
-    console.log("Your percentage grade of", grade_percentage, "corresponds to a F");
+else {
+    console.log("Your percentage grade corresponds to a F");
 }
-//Program 3: Leap Year Checker
 
-//Program 4: Largest Number Finder
+//Program 3: Leap Year Checker
+let leap = prompt("Enter a year. ");
+if ((leap % 4 == 0 && leap % 100 !== 0)|| (leap % 400 == 0)){
+    console.log("It is a Leap Year");
+}
+else{ 
+    console.log("It is not a Leap Year")
+}
+//Program 4: Largest Number Finder */
+
+let num1 = prompt("Enter a real number ");
+let num2 = prompt("Enter a different real number. ");
+let num3 = prompt("Enter another different real number ");
+//The the phrase Number below converts a value to a number
+num1 = Number(num1);
+num2 = Number(num2);
+num3 = Number(num3);
+if (num1 > num2 && num1 > num3)
+{
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+    console.log(num2);
+}
+else (num3 > num1 && num3 > num2)
+{
+    console.log(num3);
+} 
